d19a9110cc2fa9203fc064369c926ecbf118308a
zed1025 <amit251098@yahoo.in>
2022-07-31 21:55:47 +0530

    [pages] init

--- pages/pages/views.py
+++ pages/pages/views.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name: str('home.html')
+    template_name = str('home.html')
 
 
 class AboutPageView(TemplateView):
-    template_name: str('about.html')
+    template_name = str('about.html')
